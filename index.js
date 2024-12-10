233f6b20a735fd797c502df3c7cbc416b9c3ed8e
Your Name <you@example.com>
2024-12-10 15:11:45 -0500

    Working A6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "dotenv/config";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -8,7 +9,10 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
 import "dotenv/config";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
